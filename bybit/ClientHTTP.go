0d5c6f834db21619f579d0dfd62141d105badab0
Kevin Duong <1999duong.kevin@gmail.com>
2025-06-19 01:17:22 +1000

    ..

---
+++ bybit/ClientHTTP.go
@@ -0,0 +1,81 @@
+package bybit
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strconv"
+	"tradingbot/internal/fatal"
+
+	"github.com/ansel1/merry"
+)
+
+type ClientHTTP struct {
+	*http.Client
+	Scheme      string
+	Host        string
+	ByBitKey    string
+	ByBitSecret string
+}
+
+func (client *ClientHTTP) GetServerTime(ctx context.Context) (output *ServerTime, err error) {
+	target := url.URL{
+		Scheme: client.Scheme,
+		Host:   client.Host,
+		Path:   "/v5/market/time",
+	}
+	request, err := http.NewRequest(http.MethodGet, target.String(), nil)
+	fatal.OnError(err)
+	response, err := client.Do(request)
+	fatal.OnError(err)
+	if response.StatusCode != http.StatusOK {
+		err = merry.Errorf("unexpected status code (%v)", response.StatusCode).WithHTTPCode(response.StatusCode)
+		return
+	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&output)
+	return
+}
+
+func (client *ClientHTTP) GetWalletBalance(ctx context.Context, input GetWalletBalanceInput) (output *WalletBalance, err error) {
+	target := url.URL{
+		Scheme: client.Scheme,
+		Host:   client.Host,
+		Path:   "/v5/account/wallet-balance",
+	}
+	values := make(url.Values)
+	values.Set("coin", input.Coin)
+	values.Set("accountType", string(input.AccountType))
+	target.RawQuery = values.Encode()
+	request, err := http.NewRequest(http.MethodGet, target.String(), nil)
+	fatal.OnError(err)
+	timestamp := strconv.FormatInt(input.TimestampUnixMilli, 10)
+	receiveWindow := "5000"
+	signature := client.generateSignature(timestamp, receiveWindow, target.RawQuery)
+	request.Header.Set("X-BAPI-SIGN-TYPE", strconv.Itoa(2))
+	request.Header.Set("X-BAPI-SIGN", signature)
+	request.Header.Set("X-BAPI-API-KEY", client.ByBitKey)
+	request.Header.Set("X-BAPI-TIMESTAMP", timestamp)
+	request.Header.Set("X-BAPI-RECV-WINDOW", receiveWindow)
+	request.Header.Set("Content-Type", "application/json")
+	response, err := client.Do(request)
+	fatal.OnError(err)
+	if response.StatusCode != http.StatusOK {
+		err = merry.Errorf("unexpected status code (%v)", response.StatusCode).WithHTTPCode(response.StatusCode)
+		return
+	}
+	defer response.Body.Close()
+	err = json.NewDecoder(response.Body).Decode(&output)
+	return
+}
+
+func (client *ClientHTTP) generateSignature(timestamp string, receiveWindow string, input string) string {
+	payload := timestamp + client.ByBitKey + receiveWindow + input
+	mac := hmac.New(sha256.New, []byte(client.ByBitSecret))
+	mac.Write([]byte(payload))
+	return hex.EncodeToString(mac.Sum(nil))
+}
---
+++ internal/config/config.go
@@ -0,0 +1,151 @@
+// Package config Simple code to get values from the environment.
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+	"tradingbot/internal/logger"
+)
+
+// EnvStringOrFatal -- fetch a string from the environment, or Fatal if it doesn't exist.
+func EnvStringOrFatal(key string) (value string) {
+	value = os.Getenv(key)
+	if value == "" {
+		logger.Fatalf("The following environment variable must be set: %s", key)
+	}
+	return
+}
+
+// EnvInt Extract an integer value from an environment variable
+func EnvInt(key string, dflt int) int {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		logger.Fatalf("Value from environment is not an integer: key %s value %s", key, s)
+	}
+	return i
+}
+
+// EnvInt32 Extract an integer value from an environment variable
+func EnvInt32(key string, dflt int32) int32 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseInt(s, 10, 32)
+	if err != nil {
+		logger.Fatalf("Value from environment is not a 32-bit integer: key %s value %s", key, s)
+	}
+	return int32(i)
+}
+
+// EnvInt64 Extract an integer value from an environment variable
+func EnvInt64(key string, dflt int64) int64 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		logger.Fatalf("Value from environment is not a 64-bit integer: key %s value %s", key, s)
+	}
+	return i
+}
+
+// EnvUint Extract an unsigned integer value from an environment variable
+func EnvUint(key string, dflt uint) uint {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseUint(s, 10, 0)
+	if err != nil {
+		logger.Fatalf("Value from environment is not an unsigned integer: key %s value %s", key, s)
+	}
+	return uint(i)
+}
+
+// EnvUint32 Extract an integer value from an environment variable
+func EnvUint32(key string, dflt uint32) uint32 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		logger.Fatalf("Value from environment is not an unsigned 32-bit integer: key %s value %s", key, s)
+	}
+	return uint32(i)
+}
+
+// EnvUint64 Extract an integer value from an environment variable
+func EnvUint64(key string, dflt uint64) uint64 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		logger.Fatalf("Value from environment is not an unsigned 64-bit integer: key %s value %s", key, s)
+	}
+	return i
+}
+
+// EnvString Extract a string value from an environment variable
+func EnvString(key string, dflt string) (setting string) {
+	setting = os.Getenv(key)
+	if setting == "" {
+		setting = dflt
+	}
+	return
+}
+
+// EnvBool Extract a string value from an environment variable
+func EnvBool(key string, dflt bool) (setting bool) {
+	s := os.Getenv(key)
+	setting, err := strconv.ParseBool(s)
+	if err != nil {
+		setting = dflt
+	}
+	return
+}
+
+// EnvDuration Extract a duration from an environment variable
+func EnvDuration(key string, dflt time.Duration) (setting time.Duration) {
+	s := os.Getenv(key)
+	setting, err := time.ParseDuration(s)
+	if err != nil {
+		setting = dflt
+	}
+	return setting
+}
+
+// EnvFloat32 Extract a Float32 value from an environment variable
+func EnvFloat32(key string, dflt float32) float32 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		logger.Fatalf("Value from environment is not a 32-bit floating point number: key %s value %s", key, s)
+	}
+	return float32(i)
+}
+
+// EnvFloat64 Extract a Float64 value from an environment variable
+func EnvFloat64(key string, dflt float64) float64 {
+	s := os.Getenv(key)
+	if s == "" {
+		return dflt
+	}
+	i, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		logger.Fatalf("Value from environment is not a 64-bit floating point number: key %s value %s", key, s)
+	}
+	return i
+}

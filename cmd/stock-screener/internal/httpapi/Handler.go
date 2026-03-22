4131bb93ad26e1f35d15013159b188001ea27dcc
Kevin Duong <1999duong.kevin@gmail.com>
2026-03-22 10:38:05 +1000

    ..

--- cmd/stock-screener/internal/httpapi/Handler.go
+++ cmd/stock-screener/internal/httpapi/Handler.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/kduong/trading-backend/cmd/screener/internal/alpaca"
+	"github.com/kduong/trading-backend/cmd/stock-screener/internal/alpaca"
 )
 
 type Handler struct {
